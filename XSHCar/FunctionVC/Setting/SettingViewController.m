551b7b165c6e9688fec80a551c262b1c1b9a80c9
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-19 01:11:57 +0800

    设置界面实现

--- XSHCar/FunctionVC/Setting/SettingViewController.m
+++ XSHCar/FunctionVC/Setting/SettingViewController.m
@@ -9,7 +9,7 @@
 #import "SettingViewController.h"
 
 @interface SettingViewController ()
-
+@property(nonatomic, retain) NSArray *imageArray;
 @end
 
 @implementation SettingViewController
@@ -26,8 +26,98 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //设置title
+    self.title = @"设置";
+    //初始化UI
+    [self createUI];
+    //初始化数据
+    self.dataArray = (NSMutableArray *)@[@[@"通讯模式设置",@"保养矫正提示"],@[@"后台监听",@"定位开关"]];
+    self.imageArray = @[@[@"communication_mode_setting",@"maintenance_prompt_correction"],@[@"background_monitor",@"position_switch"]];
     // Do any additional setup after loading the view.
 }
+
+#pragma mark 初始化UI
+- (void)createUI
+{
+    [self addTableView];
+}
+
+- (void)addTableView
+{
+    [self addTableViewWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 44.0 *  4 + 10 * 2 + NAV_HEIGHT) tableType:UITableViewStylePlain tableDelegate:self];
+    self.table.scrollEnabled = NO;
+}
+
+#pragma mark - tableView代理
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 10.0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIImageView *imageView = [CreateViewTool createImageViewWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 10.0) placeholderImage:nil];
+    imageView.backgroundColor = [UIColor clearColor];
+    return imageView;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [self.dataArray count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[self.dataArray objectAtIndex:section] count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44.0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *homeCellID = @"settingCellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:homeCellID];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:homeCellID];
+        cell.backgroundColor = [UIColor whiteColor];
+        cell.separatorInset = UIEdgeInsetsZero;
+        cell.imageView.transform = CGAffineTransformScale(cell.imageView.transform, 0.5, 0.5);
+    }
+    
+    NSArray *array = self.dataArray[indexPath.section];
+    cell.textLabel.text = array[indexPath.row];
+    cell.textLabel.font = FONT(16.0);
+    cell.imageView.image = [UIImage imageNamed:[[self.imageArray objectAtIndex:indexPath.section] objectAtIndex:indexPath.row]];
+    cell.selectionStyle = UITableViewCellSelectionStyleDefault;
+    
+    if (indexPath.section == 1)
+    {
+        cell.selectionStyle = UITableViewCellEditingStyleNone;
+        UISwitch *switchView = [[UISwitch alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 60 , (cell.frame.size.height - 30)/2, 60, 30)];
+        [switchView setOn:NO];
+        //    switchView.onImage = [UIImage imageNamed:@"open_btn"];
+        //    switchView.offImage = [UIImage imageNamed:@"close_btn"];
+        switchView.onTintColor = RGB(28.0, 130.0, 202.0);
+        switchView.tintColor = RGB(189.0, 189.0, 189.0);
+        [cell.contentView addSubview:switchView];
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //取消选中
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
