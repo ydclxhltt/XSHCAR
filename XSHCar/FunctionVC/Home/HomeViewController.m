d73da5b78ea4898a2861c0993dd71726e053c7d4
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-19 01:10:56 +0800

    首页 界面实现

--- XSHCar/FunctionVC/Home/HomeViewController.m
+++ XSHCar/FunctionVC/Home/HomeViewController.m
@@ -9,7 +9,9 @@
 #import "HomeViewController.h"
 
 @interface HomeViewController ()
-
+{
+    float height;
+}
 @end
 
 @implementation HomeViewController
@@ -20,6 +22,7 @@
     if (self)
     {
         self.title = @"熙盛恒汽车卫士";
+        height = NAV_HEIGHT;
         // Custom initialization
     }
     return self;
@@ -28,7 +31,68 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //初始化数据
+    self.dataArray = (NSMutableArray *)@[@"一键救援",@"预约服务",@"精彩活动",@"移动商城",@"仪表盘",@"驾驶习惯",@"故障提示",@"车况检查",@"违章查询",@"油耗里程",@"位置服务",@"4S服务"];
+    //初始化UI
+    [self createUI];
+
     // Do any additional setup after loading the view.
+}
+
+#pragma mark 初始化UI
+- (void)createUI
+{
+    [self addAdvView];
+    [self addFunctionsView];
+}
+
+- (void)addAdvView
+{
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, NAV_HEIGHT, SCREEN_HEIGHT, 160)];
+    view.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:view];
+    
+    height += view.frame.size.height;
+}
+
+- (void)addFunctionsView
+{
+    int column = 4;
+    int row = ceil([self.dataArray count]/column);
+    float functionButtonHeight = 75.0;
+    float functionButtonWidth = 75.0;
+    float add_x = (SCREEN_WIDTH - functionButtonWidth * column)/5;
+    float add_y = 15.0;
+    
+    UIScrollView *scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, height, SCREEN_HEIGHT, SCREEN_HEIGHT - height - TABBAR_HEIGHT)];
+    scrollView.pagingEnabled = YES;
+    scrollView.showsHorizontalScrollIndicator = NO;
+    scrollView.showsVerticalScrollIndicator = NO;
+    scrollView.pagingEnabled = YES;
+    scrollView.contentSize = CGSizeMake(SCREEN_WIDTH, functionButtonHeight *  row + add_y *  (row + 1));
+    [self.view addSubview:scrollView];
+    
+    for (int i = 0; i < row; i++)
+    {
+        int m = column;
+        if (i == row - 1)
+        {
+            m = ([self.dataArray count]%column == 0) ? column : [self.dataArray count]%column;
+        }
+        for (int j = 0; j < m; j++)
+        {
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+            button.frame = CGRectMake(add_x * (j + 1) + functionButtonWidth * j, 20.0 + add_y * i  + functionButtonHeight * i, functionButtonWidth, functionButtonHeight);
+            button.tag = i * column + j + 1;
+            [button setTitle:self.dataArray[button.tag - 1] forState:UIControlStateNormal];
+            [button setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+            button.titleLabel.font = FONT(12.0);
+            [button setTitleEdgeInsets:UIEdgeInsetsMake(35.0, 0.0, 0.0, 0.0)];
+            [button setBackgroundImage:[UIImage imageNamed:[NSString stringWithFormat:@"icon%d",(int)button.tag]] forState:UIControlStateNormal];
+            button.showsTouchWhenHighlighted = YES;
+            [scrollView addSubview:button];
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
