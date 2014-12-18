a1a3aff8eb8d1a8beae0d91c59548c7fb3dfd826
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-19 01:11:37 +0800

    我 界面实现

--- XSHCar/FunctionVC/Mine/MineViewController.m
+++ XSHCar/FunctionVC/Mine/MineViewController.m
@@ -9,7 +9,7 @@
 #import "MineViewController.h"
 
 @interface MineViewController ()
-
+@property(nonatomic, retain) NSArray *imageArray;
 @end
 
 @implementation MineViewController
@@ -26,7 +26,84 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.title = @"我";
+    //初始化UI
+    [self createUI];
+    //初始化数据
+    self.dataArray = (NSMutableArray *)@[@[@"账户信息"],@[@"检查更新",@"帮助",@"退出"]];
+    self.imageArray = @[@[@"account_information"],@[@"check_forupdates",@"help",@"exit"]];
     // Do any additional setup after loading the view.
+}
+
+
+#pragma mark 初始化UI
+- (void)createUI
+{
+    [self addTableView];
+}
+
+- (void)addTableView
+{
+    [self addTableViewWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 44.0 *  4 + 10 * 2 + NAV_HEIGHT) tableType:UITableViewStylePlain tableDelegate:self];
+    self.table.scrollEnabled = NO;
+}
+
+
+#pragma mark - tableView代理
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 10.0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIImageView *imageView = [CreateViewTool createImageViewWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 10.0) placeholderImage:nil];
+    imageView.backgroundColor = [UIColor clearColor];
+    return imageView;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [self.dataArray count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[self.dataArray objectAtIndex:section] count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44.0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *homeCellID = @"mineCellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:homeCellID];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:homeCellID];
+        cell.backgroundColor = [UIColor whiteColor];
+        cell.separatorInset = UIEdgeInsetsZero;
+        cell.imageView.transform = CGAffineTransformScale(cell.imageView.transform, 0.5, 0.5);
+    }
+    
+    NSArray *array = self.dataArray[indexPath.section];
+    cell.textLabel.text = array[indexPath.row];
+    cell.textLabel.font = FONT(16.0);
+    cell.imageView.image = [UIImage imageNamed:[[self.imageArray objectAtIndex:indexPath.section] objectAtIndex:indexPath.row]];
+    cell.selectionStyle = UITableViewCellSelectionStyleDefault;
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //取消选中
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
