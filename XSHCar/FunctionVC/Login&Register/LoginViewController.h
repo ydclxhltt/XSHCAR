dd3435c36ef8a184756e90c137a45505012f4c15
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-24 00:38:54 +0800

    登录注册页面

---
+++ XSHCar/FunctionVC/Login&Register/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  XSHCar
+//
+//  Created by chenlei on 14/12/24.
+//  Copyright (c) 2014年 chenlei. All rights reserved.
+//
+
+#import "BasicViewController.h"
+
+@interface LoginViewController : BasicViewController
+
+@end
