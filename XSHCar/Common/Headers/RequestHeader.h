85c2d6ad3e74eb606b2f08e4ea75e3704dd7d391
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-25 02:19:38 +0800

    增加请求宏

--- XSHCar/Common/Headers/RequestHeader.h
+++ XSHCar/Common/Headers/RequestHeader.h
@@ -18,20 +18,23 @@
 //一键救援地址
 #define KEY_RESCUE_URL          [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/userlogin!keyRescue"]
 
-//精彩活动
-#define EXCITING_LIST_URL       [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/userlogin!getMessagedetail"]
-
-//移动商城
-#define MOBILE_STORE_URL        [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/userlogin!shopmall"]
-
 //预约获取个人信息
 #define BOOKING_PERSONAL_URL    [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/userlogin!appponitment"]
 
 //提交预约信息
 #define BOOKING_COMMIT_URL      [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/userlogin!addAppointment"]
 
+//精彩活动
+#define EXCITING_LIST_URL       [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/userlogin!getMessagedetail"]
+
+//移动商城
+#define MOBILE_STORE_URL        [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/userlogin!shopmall"]
+
 //故障提示
 #define TROUBLE_TIPS_URL        [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/userlogin!troubleTips"]
+
+//车况检查
+#define CAR_CHECK_URL           [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/smsInfor!medicalCondition"]
 
 //消息管理状态
 #define MESSAGE_MANAGE_URL      [NSString stringWithFormat:@"%@%@",WEB_SERVER_URL,@"/carcloud/smsInfor!getUserSetSms"]
