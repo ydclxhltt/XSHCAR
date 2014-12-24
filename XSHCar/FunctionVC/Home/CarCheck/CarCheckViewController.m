2edff7d19b883a2e0bfe48ffee81f61e1908c2f9
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-25 02:24:10 +0800

    车况体检

--- XSHCar/FunctionVC/Home/CarCheck/CarCheckViewController.m
+++ XSHCar/FunctionVC/Home/CarCheck/CarCheckViewController.m
@@ -14,10 +14,53 @@
 
 @implementation CarCheckViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    //添加backitem
+    [self addBackItem];
+    //初始化UI
+    [self createUI];
+    [self getCarCheckData];
+    
     // Do any additional setup after loading the view.
 }
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    
+}
+
+#pragma mark 初始化UI
+- (void)createUI
+{
+    
+}
+
+#pragma mark 
+- (void)getCarCheckData
+{
+    //typeof(self) weakSelf = self;
+    [SVProgressHUD showWithStatus:LOADING_DEFAULT_TIP];
+    RequestTool *request = [[RequestTool alloc] init];
+    [request requestWithUrl:CAR_CHECK_URL requestParamas:@{@"user_id":[NSNumber numberWithInt:[[XSH_Application shareXshApplication] userID]],@"car_id":[NSNumber numberWithInt:[[XSH_Application shareXshApplication] carID]]} requestType:RequestTypeSynchronous
+    requestSucess:^(AFHTTPRequestOperation *operation,id responseDic)
+    {
+         NSLog(@"loginResponseDic===%@",responseDic);
+         //if ([responseDic isKindOfClass:[NSArray class]] || [responseDic isKindOfClass:[NSMutableArray class]])
+         {
+             [SVProgressHUD showSuccessWithStatus:LOADING_SUCESS_TIP];
+         }
+         
+    }
+    requestFail:^(AFHTTPRequestOperation *operation,NSError *error)
+    {
+         [SVProgressHUD showErrorWithStatus:LOADING_FAIL_TIP];
+         NSLog(@"error===%@",error);
+    }];
+
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
