57a08c14be6a7bae5fe24f5403e792b3e180da76
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-26 00:49:58 +0800

    树形表

---
+++ XSHCar/Common/commonViews/treeCell/CLTreeView_LEVEL0_Model.m
@@ -0,0 +1,15 @@
+//
+//  CLTreeView_LEVEL0_Model.m
+//  CLTreeView
+//
+//  Created by clei on 14-9-9.
+//  Copyright (c) 2014年. All rights reserved.
+//
+
+#import "CLTreeView_LEVEL0_Model.h"
+
+@implementation CLTreeView_LEVEL0_Model
+
+@end
+
+
---
+++ XSHCar/Common/commonViews/treeCell/CLTreeView_LEVEL1_Model.h
@@ -0,0 +1,18 @@
+//
+//  CLTreeView_LEVEL1_Model.h
+//  CLTreeView
+//
+//  Created by clei on 14-9-9.
+//  Copyright (c) 2014年. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLTreeView_LEVEL1_Model : NSObject
+
+@property (strong,nonatomic) NSString *name;
+@property (strong,nonatomic) NSString *sonCnt;
+
+@end
+
+
