f0a404a28b211d0332631f52fb22207f630b9ce9
ydclxhltt <ydclxhltt@users.noreply.github.com>
2014-12-25 18:18:03 +0800

    增加自检文字

--- XSHCar/Common/Headers/CommonHeader.h
+++ XSHCar/Common/Headers/CommonHeader.h
@@ -14,18 +14,6 @@
 #import "DeviceHeader.h"
 
 
-//
-#define LOADING_DEFAULT_TIP         @"加载中..."
-
-//
-#define LOADING_SUCESS_TIP          @"加载成功"
-
-//
-#define LOADING_FAIL_TIP            @"加载失败"
-
-//
-#define LOADING_WEBERROR_TIP        @"服务器异常"
-
 //导航条高度
 #define NAV_HEIGHT                  64.0
 
@@ -49,4 +37,19 @@
 #define  APP_MAIN_COLOR             RGB(27.0,130.0,202.0)
 //tabbar背景颜色
 #define  TABBAR_BG_COLOR            RGB(12.0,97.0,152.0)
+
+//
+#define LOADING_DEFAULT_TIP         @"加载中..."
+
+//
+#define LOADING_SUCESS_TIP          @"加载成功"
+
+//
+#define LOADING_FAIL_TIP            @"加载失败"
+
+//
+#define LOADING_WEBERROR_TIP        @"服务器异常"
+
+#define CAR_CHECK_TIP               @"该OBD故障码适用于所有汽车制造商\n 新型汽车采用多阶段气囊来调节气囊内的压力。这个压力越大，气囊展开的强度就越大.\nA凸轮轴形面控制性能或卡在关闭位置 （第1排）\n躯干气囊和侧帘式气囊。侧躯干安全气囊从座椅或者门板位置展开，以对乘员的身体躯干提供保护。\n刹车助力器应用发动机进气系统（或者是专门的真空泵）产生的真空来协助制动。\nABS防抱死制动系统中，轮速传感器的作用是将车轮的速度以电信号的形式传送给控制模块.\n动态刹车控制（DBC）系统可以在急刹车情况下加强刹车的效果。。\n热氧传感器加热器控制电路 （第1排，传感器1）\n曲轴的作用是将活塞的的上下运动变成旋转运动。凸轮轴的作用是控制气门的开启和闭合.\n（在喷油嘴被广泛使用的新型汽车中，燃油量调节器已经很少被使用）"
+
 #endif
